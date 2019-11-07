0054066cc27862427bcc6e31192081f1ef9b05c9
Marshall Garey <marshall@schedmd.com>
2019-11-07 11:44:43 -0700

    Add federation on db init

--- discovery1/init_db.sh
+++ discovery1/init_db.sh
@@ -3,3 +3,4 @@ sacctmgr=bin/sacctmgr
 $sacctmgr -i add cluster discovery1 discovery2 discovery3
 $sacctmgr -i add account acct1
 $sacctmgr -i add user marshall account=acct1
+$sacctmgr -i add fed discovery clusters=discovery1,discovery2,discovery3
