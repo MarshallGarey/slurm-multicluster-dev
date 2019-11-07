45e7fbca2bdcc6390796eaa39ae4a0198c1954e0
Marshall Garey <marshall@schedmd.com>
2019-11-07 16:45:12 -0700

    Don't print error if no files with *.pid are found Adding .gitignore to the run/ directories meant my previous method of finding out of the run/ directories were empty didn't work anymore (because they aren't empty anymore). Now I just search for files not named '.gitignore' and if I find any, operate on all of those.

--- stop_clusters.sh
+++ stop_clusters.sh
@@ -13,15 +13,15 @@ basepath="/home/marshall/slurm/master/install"
 echo "stopping clusters discovery1, discovery2, discovery3, base=$basepath"
 
 # Are there any files in the directory (i.e., daemons are running)?
-# -prune means don't descend into the directory
-# -empty means "file is empty and is either a regular file or directory"
-if [ -z "$(find $basepath/run -prune -empty)" ]
+# -n flag for if means is not null
+# -z flag for if means is null
+if [ -n "`find $basepath/run ! -name '.gitignore' -type f`" ]
 then
 	for pid in `cat $basepath/run/*.pid`; do sudo kill $pid; done
 fi
 for i in {1..3}
 do
-	if [ -z "$(find $basepath/discovery$i/run -prune -empty)" ]
+	if [ -n "`find $basepath/run ! -name '.gitignore' -type f`" ]
 	then
 		for pid in `cat $basepath/discovery$i/run/*.pid`; do sudo kill $pid; done
 	fi
