8bf2efef9666c1850b2133bfbb22c7f9e6916481
Marshall Garey <marshall@schedmd.com>
2020-12-11 14:38:28 -0700

    Improve start_clusters script.
    Only start cluster c1 by default. Add an optional argument to specify
    how many clusters to start. Enforce that argument to be 1, 2, or 3.

--- start_clusters.sh
+++ start_clusters.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+# start_clusters.sh
+# Startup script for Slurm
+# Usage:
+#  ./start_clusters.sh [<number_of_clusters>]
+# number_of_clusters is optional. If not given, then 1 cluster will start.
+# Restrict number_of_clusters to between 1 and 3, since 1 is the minimum
+# and 3 is how many clusters I have configured, so 3 is the maximum.
 
 # Ensure that we have sudo privileges
 sudo -v
@@ -9,24 +16,55 @@ then
 	exit 1
 fi
 
-basepath="/home/#USER/slurm/#VERSION/install"
-echo "starting clusters c1, c2, c3, base=$basepath"
+# $# is the number of arguments passed to the script
+if [ $# -ne 0 ]
+then
+	is_not_num_regex='[^0-9]+'
+	if [[ $1 =~ $is_not_num_regex || $1 -ge 4 || $1 -le 0 ]]
+	then
+		echo "Error: Invalid argument ($1) - it must be 1, 2, or 3"
+		exit 1
+	fi
+	num_clusters=$1
+else
+	num_clusters=1
+fi
+
+installpath="/home/#USER/slurm/#VERSION/install"
+
+if [ $num_clusters -eq 1 ]
+then
+	echo "Starting cluster c1"
+else
+	echo "Starting clusters c[1-$num_clusters]"
+fi
 
 # Start slurmdbd
-sudo -u #USER $basepath/sbin/slurmdbd
+sudo -u #USER $installpath/sbin/slurmdbd
 sleep 1
 
 # Start slurmctld's
-for i in {1..3}
+i=1
+while [ $i -le $num_clusters ]
 do
-	SLURM_CONF="$basepath/c$i/etc/slurm.conf"
-	sudo -u #USER SLURM_CONF=$SLURM_CONF $basepath/sbin/slurmctld -f $SLURM_CONF
+	SLURM_CONF="$installpath/c$i/etc/slurm.conf"
+	sudo -u #USER SLURM_CONF=$SLURM_CONF $installpath/sbin/slurmctld -i -f $SLURM_CONF
+	i=$(($i+1))
 done
 
 # Start slurmd's
-for i in {1..10}
+num_nodes=10
+cluster_inx=1
+while [ $cluster_inx -le $num_clusters ]
 do
-	sudo $basepath/sbin/slurmd -f $basepath/c1/etc/slurm.conf -N n1-$i
-	sudo $basepath/sbin/slurmd -f $basepath/c2/etc/slurm.conf -N n2-$i
-	sudo $basepath/sbin/slurmd -f $basepath/c3/etc/slurm.conf -N n3-$i
+	node_inx=1
+	#SLURM_CONF="$installpath/c$cluster_inx/etc/slurm.conf"
+	SLURM_CONF="$installpath/c$cluster_inx/etc/slurm.conf"
+	while [ $node_inx -le $num_nodes ]
+	do
+		echo "Start node n$cluster_inx-$node_inx"
+		sudo $installpath/sbin/slurmd -f $SLURM_CONF -N n$cluster_inx-$node_inx
+		node_inx=$(($node_inx+1))
+	done
+	cluster_inx=$(($cluster_inx+1))
 done
