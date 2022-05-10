6bb077c7cae8b213bf4a4dcf74523ef9607dc68c
Marshall Garey <marshall@schedmd.com>
2022-05-10 16:47:17 -0600

    Add powersave scripts

---
+++ c1/scripts/resumefailprog.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+log_file="/home/marshall/slurm/master/install/c1/log/powersave.log"
+# Redirect all stdout/stderr to a log file
+exec 2>&1
+exec 1>>$log_file
+
+echo "RESUME FAIL PROGRAM: ran at: `date`"
+echo "All args:"
+echo $@
+printenv
+echo ""
+echo ""
