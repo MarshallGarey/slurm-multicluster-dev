b80587308d132ee80a6e28976e7b7e0505db23f3
Marshall Garey <marshall@schedmd.com>
2022-05-10 14:14:19 -0600

    Add a useful MailProg

---
+++ c3/scripts/mail.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+PREFIX=/home/#USER/slurm/#VERSION/install/c3/
+LOG=$PREFIX/log/mail.log
+
+# Redirect all stdout and stderr to a log file!
+exec 2>&1
+exec 1>>$LOG
+
+now=$(date)
+
+echo "Mail $SLURM_JOB_MAIL_TYPE: ******************************************"
+echo "Mail $SLURM_JOB_MAIL_TYPE: Job: $SLURM_JOB_ID"
+echo "Mail $SLURM_JOB_MAIL_TYPE: Date: $now"
+echo "Mail $SLURM_JOB_MAIL_TYPE: User: $SLURM_JOB_USER"
+echo "Mail $SLURM_JOB_MAIL_TYPE: Mail Type: $SLURM_JOB_MAIL_TYPE"
+echo "Mail $SLURM_JOB_MAIL_TYPE: Job State: $SLURM_JOB_STATE"
+echo "Mail $SLURM_JOB_MAIL_TYPE: ******************************************"
+echo ""
+echo ""
+
