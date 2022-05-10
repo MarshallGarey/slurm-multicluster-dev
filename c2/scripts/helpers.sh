eecede528b10cf3b10d497e6fe20bd22bc8a2140
Marshall Garey <marshall@schedmd.com>
2022-05-10 17:09:05 -0600

    Correct various paths
    Replace instances of "marshall" or "master" in paths with the conf
    values (#USER and #VERSION).
    Also fix various instances of having the incorrect cluster directory in
    a path.

--- c2/scripts/helpers.sh
+++ c2/scripts/helpers.sh
@@ -3,7 +3,7 @@
 #echo "hf2"
 
 
-helpers_variable_file=/home/marshall/slurm/master/install/c1/scripts/helpers_variables
+helpers_variable_file=/home/#USER/slurm/#VERSION/install/c2/scripts/helpers_variables
 touch $helpers_variable_file
 if [ -n \"$1\" ]; then
 	echo $1 > $helpers_variable_file
--- c3/scripts/suspendprog.sh
+++ c3/scripts/suspendprog.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-log_file="/home/marshall/slurm/master/install/c3/log/powersave.log"
+log_file="/home/#USER/slurm/#VERSION/install/c3/log/powersave.log"
 # Redirect all stdout/stderr to a log file
 exec 2>&1
 exec 1>>$log_file
