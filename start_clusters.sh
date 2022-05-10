f7c2fa90612a477c944ca2411a75f6918256fcba
Marshall Garey <marshall@schedmd.com>
2022-05-10 15:46:49 -0600

    Put option in a flag instead.

--- start_clusters.sh
+++ start_clusters.sh
@@ -6,6 +6,30 @@
 # number_of_clusters is optional. If not given, then 1 cluster will start.
 # Restrict number_of_clusters to between 1 and 3, since 1 is the minimum
 # and 3 is how many clusters I have configured, so 3 is the maximum.
+num_clusters=1
+
+print_usage() {
+	printf "Usage: ./start_clusters.sh [-c<num_clusters>]\n"
+}
+
+while getopts 'c:u' flag
+do
+	case "${flag}" in
+		c) num_clusters=${OPTARG} ;;
+		u) print_usage
+		   exit 1 ;;
+	esac
+done
+
+# Validate num_clusters
+echo "num_clusters=$num_clusters"
+
+is_not_num_regex='[^0-9]+'
+if [[ $num_clusters =~ $is_not_num_regex || $num_clusters -ge 4 || $num_clusters -le 0 ]]
+then
+	echo "Error: Invalid argument ($1) - it must be 1, 2, or 3"
+	exit 1
+fi
 
 # Ensure that we have sudo privileges
 sudo -v
@@ -14,20 +38,6 @@ if [ $rc != 0 ]
 then
 	echo "failed, need sudo privileges to run this script"
 	exit 1
-fi
-
-# $# is the number of arguments passed to the script
-if [ $# -ne 0 ]
-then
-	is_not_num_regex='[^0-9]+'
-	if [[ $1 =~ $is_not_num_regex || $1 -ge 4 || $1 -le 0 ]]
-	then
-		echo "Error: Invalid argument ($1) - it must be 1, 2, or 3"
-		exit 1
-	fi
-	num_clusters=$1
-else
-	num_clusters=1
 fi
 
 installpath="/home/#USER/slurm/#VERSION/install"
