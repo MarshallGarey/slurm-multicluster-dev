e95a25aa0d987c46f213ffd2642b5640aa2422af
Marshall Garey <marshall@schedmd.com>
2022-05-10 18:29:17 -0600

    Create globals.local file to enable running the testsuite

--- init.sh
+++ init.sh
@@ -13,6 +13,21 @@ chmod 775 make.py
 cd ..
 ./setup_bin.sh
 
+# Setup testsuite
+printf '# globals.local
+set base "/home/#USER/slurm/#VERSION"
+set slurm_dir "${base}/install"
+set build_dir "${slurm_dir}/build"
+#set testsuite_log_level $LOG_LEVEL_TRACE
+set testsuite_cleanup_on_failure false
+
+#federation tests (test37.*) are all broken right now
+#set fed_slurm_base "$slurm_dir"
+#set fedc1 "c1"
+#set fedc2 "c2"
+#set fedc3 "c3"
+' > ../slurm/testsuite/expect/globals.local
+
 # Setup database
 mkdir archive
 export SLURM_CONF=`pwd`/etc/slurm.conf
