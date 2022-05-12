74b4617f18b36f66f46f078878d2cb6776529328
Marshall Garey <marshall@schedmd.com>
2022-05-12 09:54:09 -0600

    Use updated make.py

--- init.sh
+++ init.sh
@@ -6,7 +6,7 @@ mkdir build lib
 cd build
 ../../slurm/configure --prefix=/home/#USER/slurm/#VERSION/install --enable-developer --enable-multiple-slurmd --disable-optimizations --with-pam_dir=/home/#USER/slurm/#VERSION/install/lib
 # Install: first get make.py, then make.
-wget https://gitlab.com/bsngardner/slurm_devinst_scripts/-/raw/master/make.py
+wget https://gitlab.com/SchedMD/support/scripts/-/raw/master/make.py
 chmod 775 make.py
 ./make.py --with-all
 # Setup bin directories for each cluster
