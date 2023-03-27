6403f7bf95909324baff3192b2e33a1cf7631c00
Marshall Garey <marshall@schedmd.com>
2023-03-27 13:34:29 -0600

    Fix slurm.conf path

--- start_node.sh
+++ start_node.sh
@@ -3,6 +3,6 @@ set -x
 nodename=$1
 cluster=$2
 args=$3
-export SLURM_CONF="$(pwd)/${cluster}/slurm.conf"
+export SLURM_CONF="$(pwd)/${cluster}/etc/slurm.conf"
 export NODE_NAME=${nodename}
 sbin/slurmd -N$nodename ${args}
