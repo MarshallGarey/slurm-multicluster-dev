ec3ac3dc22cbca7244c05fb210d7034662520aa8
Marshall Garey <marshall@schedmd.com>
2023-04-17 11:26:19 -0600

    start_node.sh - Add usage and move sudo inside the script

--- start_node.sh
+++ start_node.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
+if [ $# -lt 2 ]
+then
+	echo "Usage: ./start_node.sh nodename clustername [slurmd args]"
+	exit 1
+fi
 set -x
 nodename=$1
 cluster=$2
 args=$3
 export SLURM_CONF="$(pwd)/${cluster}/etc/slurm.conf"
 export NODE_NAME=${nodename}
-sbin/slurmd -N$nodename ${args}
+sudo --preserve-env=SLURM_CONF,NODE_NAME "$(pwd)/sbin/slurmd" -N$nodename ${args}
