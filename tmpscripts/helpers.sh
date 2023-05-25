a2f969cb1634bf4c66365977ecb4f08433422b83
Marshall Garey <marshall@schedmd.com>
2023-05-25 07:00:29 -0600

    Fix setting active features in helpers.sh

--- tmpscripts/helpers.sh
+++ tmpscripts/helpers.sh
@@ -7,15 +7,17 @@ if [ $# -eq 1 -a -n \"$1\" ]
 then
 	# Append features
 	echo $1 >> ${helpers_variable_file}
+	features=$(cat "${helpers_variable_file}")
 else
 	# Clear features
+	features=$(cat "${helpers_variable_file}")
+	echo "${features}"
 	printf "" > ${helpers_variable_file}
 fi
-cat ${helpers_variable_file}
 
 printf "Run at $(date) with args: '$@'
 NODE_NAME=$(printenv NODE_NAME)
 helpers_variable_file=${helpers_variable_file}
 cat ${helpers_variable_file}=
-$(cat ${helpers_variable_file})
+${features}
 \n" >> $log
