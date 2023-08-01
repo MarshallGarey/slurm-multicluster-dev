MarshallGarey/slurm-multicluster-dev

commit ba3fcea10e001576968d2c60af5662b9466116bb

Rework init code to dynamically create cluster directories
This allows removing the cluster directories from the repository and
adding them to .gitignore. That way any changes made in any of these
directories are ignored, and the output of git commands like git status
will be a lot cleaner. Hopefully it will be easier to pull changes.

init.sh
@@ -30,15 +30,22 @@ then
 else
 	${makeme} --with-all
 fi
-# Setup bin directories for each cluster
-cd ..
-slurm_path="$(pwd)"
+# Setup directories for each cluster
+cd "${install_path}"
 i=1
 while [ $i -le 3 ]
 do
-	c="${slurm_path}/c$i"
+	c="${install_path}/c$i"
 	#echo $c
-	mkdir ${c}/bin
+	# Make the cluster directory and any additional needed directories.
+	mkdir "${c}"
+	cd "${c}"
+	mkdir log
+	mkdir run
+	mkdir spool
+	# Setup the bin directory; each script will be a wrapper of the
+	# actual binary file
+	mkdir bin
 	for file in $(ls bin/)
 	do
 		script="${c}/bin/${file}"
@@ -46,11 +53,15 @@ do
 		printf "\
 #!/bin/sh
 export SLURM_CONF=${c}/etc/slurm.conf
-exec ${slurm_path}/bin/${file} \"\$@\"
+exec ${install_path}/bin/${file} \"\$@\"
 " > $script
 		chmod 775 $script
 	done
 	i=$((${i}+1))
+	# Setup symlinks
+	ln -sr ../sbin sbin
+	ln -sr ../share share
+	ln -sr ../.envrc .envrc
 done
 
 # Copy example scripts or conf files from the Slurm repo etc directory

.gitignore
@@ -7,3 +7,6 @@ sbin/*
 man/*
 */state/*
 slurm-*.out
+c1
+c2
+c3

c1/.envrc
@@ -1,1 +1,0 @@
-../.envrc

c1/log/.gitignore
@@ -1,2 +1,0 @@
-*
-!.gitignore

c1/run/.gitignore
@@ -1,2 +1,0 @@
-*
-!.gitignore

c1/sbin
@@ -1,1 +1,0 @@
-../sbin

c1/share
@@ -1,1 +1,0 @@
-../share

c1/spool/.gitignore
@@ -1,2 +1,0 @@
-*
-!.gitignore

commit 1e26e6b023410647d8676da92706b927f2af49bf

Fix setting up bin directories
Regression from commit ba3fcea

init.sh
@@ -45,6 +45,7 @@ do
 	# Setup the bin directory; each script will be a wrapper of the
 	# actual binary file
 	mkdir bin
+	cd "${install_path}"
 	for file in $(ls bin/)
 	do
 		script="${c}/bin/${file}"
@@ -58,6 +59,7 @@ exec ${install_path}/bin/${file} \"\$@\"
 	done
 	i=$((${i}+1))
 	# Setup symlinks
+	cd "${c}"
 	ln -sr ../sbin sbin
 	ln -sr ../share share
 	ln -sr ../.envrc .envrc