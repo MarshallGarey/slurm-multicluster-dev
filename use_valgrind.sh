0abb9b3c04a1e0b3a640ba9d293ecd93f85d7558
Marshall Garey <marshall@schedmd.com>
2025-01-20 15:50:08 -0700

    use_valgrind.sh - Add executable name and timestamp to output filename
    This allows running use_valgrind.sh on multiple binaries at the same time,
    without those separate valgrind instances writing to the same filename.

--- use_valgrind.sh
+++ use_valgrind.sh
@@ -5,11 +5,16 @@ then
 	echo "./use_valgrind.sh slurmctld -D -vvv"
 	exit 1
 fi
+# Get timestamp in seconds.nanoseconds
+tstampms="$(date +%s.%N)"
+# Generate a hopefully unique filename
+fname="valgrind-out-$(basename ${1})-${tstampms}.txt"
+echo "Valgrind output filename: ${fname}"
 valgrind --leak-check=full \
          --show-leak-kinds=all \
          --track-origins=yes \
 	 --trace-children=yes \
 	 --keep-debuginfo=yes \
          --verbose \
-         --log-file=valgrind-out.txt \
+	 --log-file="${fname}" \
 	 $@
